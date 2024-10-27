9d50731f5ca8f9637bb5b16e6141303b9fc781f6
Louis Ward <louis.ward@ocgsoftware.com>
2024-10-27 16:04:43 +0000

    lombokacus

--- src/main/java/org/com/entity/User.java
+++ src/main/java/org/com/entity/User.java
@@ -1,11 +1,15 @@
 package org.com.entity;
 
 import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.envers.Audited;
 
 
 @Entity
 @Audited
+@Getter
+@Setter
 public class User extends BaseEntity {
     public String username;
     public String password;
