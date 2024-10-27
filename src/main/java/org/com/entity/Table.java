3bffc872e923439719c4f517c3455990e4a231d4
Louis Ward <louis.ward@ocgsoftware.com>
2024-10-27 16:27:11 +0000

    FieldUpdateRequest

--- src/main/java/org/com/entity/Table.java
+++ src/main/java/org/com/entity/Table.java
@@ -1,6 +1,7 @@
 package org.com.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.envers.Audited;
@@ -14,5 +15,6 @@ import java.util.List;
 @Setter
 public class Table extends BaseEntity {
     private String name;
+    @OneToMany
     private List<Field> fields;
 }
