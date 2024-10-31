879d22be863b2b822ad1f32ac65cc3126a539007
louis.ward <louis.ward@ocgsoftware.com>
2024-10-31 13:28:58 +0000

    testing

--- src/test/java/org/com/auth/AuthTest.java
+++ src/test/java/org/com/auth/AuthTest.java
@@ -3,18 +3,20 @@ package org.com.auth;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 import jakarta.ws.rs.core.Response;
+import org.com.entity.User;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 
 @QuarkusTest
 public class AuthTest {
+    private static final String AUTH_REQUEST_JSON = "{\"username\": \"liljohn\", \"password\": \"maryjane\"}";
     
     @Test
     public void testInvalidCredentials() {
         given()
             .contentType(ContentType.JSON)
-            .body("{\"username\": \"liljohn\", \"password\": \"maryjane\"}")
+            .body(AUTH_REQUEST_JSON)
             .when()
             .post("/auth/token")
             .then()
@@ -25,17 +27,19 @@ public class AuthTest {
     public void testRegister() {
         given()
             .contentType(ContentType.JSON)
-            .body("{\"username\": \"liljohn\", \"password\": \"maryjane\"}")
+            .body(AUTH_REQUEST_JSON)
             .when()
             .post("/auth/register")
             .then()
             .statusCode(Response.Status.CREATED.getStatusCode());
         given()
             .contentType(ContentType.JSON)
-            .body("{\"username\": \"liljohn\", \"password\": \"maryjane\"}")
+            .body(AUTH_REQUEST_JSON)
             .when()
             .post("/auth/token")
             .then()
             .statusCode(Response.Status.OK.getStatusCode());
+        System.out.println(User.count());
+        System.out.println("test");
     }
 }
