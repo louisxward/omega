96abd06e1394fd0713aac00e289e8e8dbdbbde25
Louis Ward <louis.ward@ocgsoftware.com>
2024-11-06 20:07:48 +0000

    sysouts

--- src/main/java/org/com/api/FileUploadResource.java
+++ src/main/java/org/com/api/FileUploadResource.java
@@ -1,12 +1,15 @@
 package org.com.api;
 
 import jakarta.inject.Inject;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.com.exception.ValidationException;
 import org.com.model.FileUploadForm;
 import org.jboss.logging.Logger;
 
@@ -20,10 +23,24 @@ public class FileUploadResource {
     @POST
     @Path("/upload")
     @Consumes(MediaType.MULTIPART_FORM_DATA)
-    public Response uploadFile(FileUploadForm form) {
+    public Response uploadFile(@NotNull @Valid FileUploadForm form) {
         logger.info("uploadFile");
-        System.out.println("fileName: " + form.fileName);
-        System.out.println("fileSize: " + form.file.size());
+        var fileSize = form.file.size();
+        if (fileSize < 1L) {
+            throw new ValidationException("file", "Please upload a file");
+        }
+        if (fileSize > 2 * 1024L * 1024L) {
+            throw new ValidationException("file", "File is too bigg");
+        }
+        var file = form.file;
+        System.out.println("Req fileName: " + form.fileName);
+        System.out.println("fileName: " + file.fileName());
+        System.out.println("fileSize: " + file.size());
+        System.out.println("filePath: " + file.filePath());
+        System.out.println("name: " + file.name());
+        System.out.println("name: " + file.contentType());
+        // more checks
+        
         return Response.ok().build();
     }
 }
--- src/main/java/org/com/model/FileUploadForm.java
+++ src/main/java/org/com/model/FileUploadForm.java
@@ -1,11 +1,15 @@
 package org.com.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.jboss.resteasy.reactive.RestForm;
 import org.jboss.resteasy.reactive.multipart.FileUpload;
 
 public class FileUploadForm {
     @RestForm
+    @NotNull
     public FileUpload file;
     @RestForm
+    @NotBlank
     public String fileName;
 }
